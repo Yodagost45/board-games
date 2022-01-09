c485e765084eafdb83d2f8b0699e6e3d7084238f
Yodagost45 <jonathan78564@gmail.com>
2022-01-09 19:24:41 +0000

    Started frontend

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>The count is: {{ count }}</p>
+    <b-button variant="success" @click="addCount()">Click Me!</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+    },
+  },
+});
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Counter msg="boop a doo" />
+    <Counter msg="boop a doo" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Counter from "@/components/Counter.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Counter,
+  },
+});
+</script>
